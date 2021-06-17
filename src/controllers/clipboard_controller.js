ac8baff32289a62c9354ac4d3ab284d52e1903ad
Simon Tharby <simon@liquidvoting.io>
2021-06-17 11:13:42 +0100

    Building something real example
    Copy read only text from input field or text element, using
    button or link (with default behaviour prevented).

---
+++ src/controllers/clipboard_controller.js
@@ -0,0 +1,12 @@
+// src/controllers/clipboard_controller.js
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "source" ]
+
+  copy() {
+    event.preventDefault()
+    this.sourceTarget.select()
+    document.execCommand("copy")
+  }
+}
